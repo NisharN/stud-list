8dc42ec10316f06d0f4c5ca6d2c2a5b4c7a66567
NisharN <nisharr03@gmail.com>
2024-11-24 12:20:29 +0530

    new commit

---
+++ src/assets/assets.js
@@ -0,0 +1,22 @@
+import breakingbad from './breakingbad.jpg';
+import gameOfThrones from './got.jpg';
+import friends from './friends.jfif';
+import moneyheist from './money.jpg';
+import peakyblinders from './peaky.jfif';
+import office from './office.jfif';
+import strangerthings from './strangerthings.jpg';
+import narcos from './narcos.jpg';
+import dark from './dark.jpg';
+
+const assets={
+    breakingbad,
+    gameOfThrones,
+    friends,
+    moneyheist,
+    peakyblinders,
+    office,
+    strangerthings,
+    narcos,
+    dark
+}
+export default assets;
